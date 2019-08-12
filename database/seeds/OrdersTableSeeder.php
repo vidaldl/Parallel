517811748e7aee3e82675a6d16f66630642ed8ab
vagrant <vagrant@homestead>
2019-08-12 05:42:29 +0000

    testagain1

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -12,61 +12,53 @@ class OrdersTableSeeder extends Seeder
      */
     public function run()
     {
-        
 
-        \DB::table('orders')->delete();
-        
-        \DB::table('orders')->insert(array (
-            0 => 
+
+
+
+        DB::table('orders')->insert(array (
+            0 =>
             array (
-                'id' => 1,
                 'section' => 'servicios',
                 'order' => 0,
             ),
-            1 => 
+            1 =>
             array (
-                'id' => 2,
                 'section' => 'infoslider1',
                 'order' => 1,
             ),
-            2 => 
+            2 =>
             array (
-                'id' => 3,
                 'section' => 'infoslider2',
                 'order' => 2,
             ),
-            3 => 
+            3 =>
             array (
-                'id' => 4,
                 'section' => 'infoslider3',
                 'order' => 3,
             ),
-            4 => 
+            4 =>
             array (
-                'id' => 5,
                 'section' => 'pricing',
                 'order' => 4,
             ),
-            5 => 
+            5 =>
             array (
-                'id' => 6,
                 'section' => 'articulos',
                 'order' => 5,
             ),
-            6 => 
+            6 =>
             array (
-                'id' => 7,
                 'section' => 'info',
                 'order' => 6,
             ),
-            7 => 
+            7 =>
             array (
-                'id' => 8,
                 'section' => 'contact',
                 'order' => 7,
             ),
         ));
-        
-        
+
+
     }
 }
