1ed8cee39f667e720c6f37b32f34b2e899de03c1
vagrant <vagrant@homestead>
2020-02-04 18:55:56 +0000

    Revert "icons added"
    This reverts commit 5c5dbb7d29061f0a0eb67dd3d3e2481636c2d932.
    Icons added reverted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
 
   <!-- Custom fonts for this template-->
 
-  <!-- <link href="{{ asset('css/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css"> -->
-  <link href="{{ asset('lib/fontawesome/css/all.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
@@ -268,7 +267,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-  <!-- <script src="{{ asset('lib/fontawesome/js/all.css') }}"></script> -->
+  <!-- Core plugin JavaScript-->
 
 
   <!-- Custom scripts for all pages-->
