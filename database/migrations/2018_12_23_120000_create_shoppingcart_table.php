c7a59cd0afe08b5c2352d22e884b339a12551a72
vagrant <vagrant@homestead>
2020-06-02 16:00:32 +0000

    migration fixed

--- database/migrations/2018_12_23_120000_create_shoppingcart_table.php
+++ database/migrations/2018_12_23_120000_create_shoppingcart_table.php
@@ -12,7 +12,7 @@ class CreateShoppingcartTable extends Migration
     public function up()
     {
         Schema::create(config('cart.database.table'), function (Blueprint $table) {
-            $table->string('identifier');
+            $table->string('identifier', 250);
             $table->string('instance');
             $table->longText('content');
             $table->nullableTimestamps();
